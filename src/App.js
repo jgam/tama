4f4d928b27e03c2b7be2a6f65373e525e210ee65
jgam <jgam@alumni.nd.edu>
2019-08-06 13:30:24 +0900

    architecture done

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component{
       count: 'bye app js!'
     });
   };
-
+  //<!--thi s needs to be changed with different conditions and call different criteria-->
   render() {
     return (
       <div className="App">
@@ -21,12 +21,8 @@ class App extends Component{
           {this.state.count}
         </div>
         <button onClick={this.handleChange}>click me!</button>
-        <div>N5</div>
-        <div>N4</div>
-        <div>N3</div>
-        <div>N2</div>
-        <div>N1</div>
-
+       
+        {this.render_title_page()}
         <div className="detail">this is detail now</div>
 
       </div>
@@ -34,12 +30,26 @@ class App extends Component{
   }
 
   render_title_page(){
+    //this needs to return states for passing different values
+    return(
+      <div>
+        <button onClick={() => this.render_view_level()}>N5</button>
+        <div>N4</div>
+        <div>N3</div>
+        <div>N2</div>
+        <div>N1</div>
+      </div>
+    )
 
   }
 
   //here we need props to set the condition
   render_view_level(){
-
+    
+    console.log('hello world');
+    return(
+      <div>hello world</div>
+    )
   }
 }
 export default App;
